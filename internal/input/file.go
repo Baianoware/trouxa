a89f95032b09417ae08f675cb613c97bef880668
Henry Barreto <contato@henrybarreto.dev>
2022-02-19 21:59:11 -0300

    Removed unnecessary function

--- internal/input/file.go
+++ internal/input/file.go
@@ -4,10 +4,6 @@ import (
 	"io/ioutil"
 )
 
-func loadFile(path string) ([]byte, error) {
-	return ioutil.ReadFile(path)
-}
-
 func getPackagesFromFile(packagesPath string) ([]byte, error) {
-	return loadFile(packagesPath)
+	return ioutil.ReadFile(packagesPath)
 }
