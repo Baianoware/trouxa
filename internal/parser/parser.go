fa17a8a45866c63b4af9379f11446e31bdc988fb
Henry Barreto <contato@henrybarreto.dev>
2022-02-26 00:16:32 -0300

    Simplifying parser logic

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,43 +1,39 @@
 package parser
 
 import (
+	"log"
+	"regexp"
 	"strings"
 
 	"github.com/Bainoware/trouxa/internal/entities"
 )
 
-func splitLines(data []byte) []string {
-	return strings.Split(string(data), "\n")
-}
-
-func splitName(line string) string {
-	return strings.Split(line, "=")[0]
-}
-
-func splitVersion(line string) string {
-	return strings.Split(line, "=")[1]
-}
-
 // Parse the packages' list to entities.Packages
 func Parse(data []byte) []entities.Package {
-	addPackageNameVersion := func(packages *[]entities.Package, line string) {
-		*packages = append(*packages, entities.Package{Name: splitName(line), Version: splitVersion(line)})
-	}
-	addPackageName := func(packages *[]entities.Package, line string) {
-		*packages = append(*packages, entities.Package{Name: splitName(line)})
-	}
-	lines := splitLines(data)
-	var packages []entities.Package
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		if line == "" {
-			continue
+	// Gets all lines.
+	lines := strings.Split(string(data), "\n")
+	rule := regexp.MustCompile("^([a-zA-Z])*=?([0-9.])*$")
+
+	packages := make([]entities.Package, 0)
+	for index, line := range lines {
+		// Valid if a line has the right format.
+		// package=version
+		if !rule.MatchString(line) {
+			log.Fatalf("line %d, %s, has a invalid format", index, line)
 		}
-		if strings.ContainsRune(line, '=') && splitVersion(line) != "" {
-			addPackageNameVersion(&packages, line)
+
+		// if package contains version indicator, put the version on package's structure.
+		if strings.ContainsRune(line, '=') {
+			name := strings.TrimSpace(strings.Split(lines[index], "=")[0])
+			version := strings.TrimSpace(strings.Split(lines[index], "=")[1])
+
+			packages = append(packages, entities.Package{Name: name, Version: version})
 		} else {
-			addPackageName(&packages, line)
+			name := strings.TrimSpace(strings.Split(lines[index], "=")[0])
+
+			packages = append(packages, entities.Package{Name: name})
 		}
 	}
+
 	return packages
 }
--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -6,20 +6,6 @@ import (
 	"github.com/Bainoware/trouxa/internal/entities"
 )
 
-func TestSplitName(t *testing.T) {
-	name := splitName("vim=1.0")
-	if name != "vim" {
-		t.Fail()
-	}
-}
-
-func TestSplitVersion(t *testing.T) {
-	version := splitVersion("vim=1.0")
-	if version != "1.0" {
-		t.Fail()
-	}
-}
-
 func TestParser(t *testing.T) {
 	data := Parse([]byte("vim=1.0\npython=3\nnano\nmysql="))
 	expected := []entities.Package{
