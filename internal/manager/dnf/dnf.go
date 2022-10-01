bd6f403a7d54ca99abef75f33841a25fdc109b69
Carlos Robles <carlos.robles@gmail.com>
2022-10-01 12:01:41 -0500

    add packages managers for aptitude, dns, snap, yum, and zypper

---
+++ internal/manager/dnf/dnf.go
@@ -0,0 +1,18 @@
+package dnf
+
+import (
+	"os/exec"
+)
+
+type Commander struct {
+}
+
+// BuildInstallCommand create the installation command to dnf
+func (p *Commander) BuildInstallCommand(name string) *exec.Cmd {
+	return exec.Command("dnf", "install", "-y", name)
+}
+
+// BuildUninstallCommand create the uninstallation command to dnf
+func (p *Commander) BuildUninstallCommand(name string) *exec.Cmd {
+	return exec.Command("dnf", "remove", "-y", name)
+}
